bc61ad8f305d11e6a6278579926821ed630c9b00
Prakash Ravichandran <prakash.ravichandran@hashouttech.com>
2021-05-19 15:48:31 +0530

    gulp tech

---
+++ Day7/gulpfile.js
@@ -0,0 +1,31 @@
+
+var gulp = require("gulp"),//https://gulpjs.com/
+	util = require("gulp-util"),//https://github.com/gulpjs/gulp-util
+	sass = require("gulp-sass"),//https://www.npmjs.org/package/gulp-sass
+	autoprefixer = require('gulp-autoprefixer'),//https://www.npmjs.org/package/gulp-autoprefixer
+	minifycss = require('gulp-minify-css'),//https://www.npmjs.org/package/gulp-minify-css
+	rename = require('gulp-rename'),//https://www.npmjs.org/package/gulp-rename
+	log = util.log;
+
+
+    
+gulp.task('default', function() {
+    // place code for your default task here
+    log("Generate CSS files " + (new Date()).toString());
+   return gulp.src('scss/*.scss')
+		.pipe(sass({ style: 'expanded' }))
+					.pipe(autoprefixer("last 3 version","safari 5", "ie 8", "ie 9"))
+		.pipe(gulp.dest("target/css"))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(minifycss())
+		.pipe(gulp.dest('target/css'));
+
+  });
+
+
+  gulp.task("watch", function(){
+	log("Watching scss files for modifications");
+	gulp.watch('scss/*.scss', ["sass"]);
+});
+
+ 
